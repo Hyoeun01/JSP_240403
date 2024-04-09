aa0f41a9413d2ae08c26b5bdbe579bd2b8095a27
Hyoeun01 <shyoeun09@gmail.com>
2024-04-09 20:25:30 +0900

    jsp test 페이지 확인 , MyServlet 코드 설명 추가

--- w1/src/main/java/org/zerock/w1/MyServlet.java
+++ w1/src/main/java/org/zerock/w1/MyServlet.java
@@ -8,14 +8,18 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+// @WebServlet : 브라우저의 경로와 해당 서블릿을 연결
 @WebServlet(name = "myServlet", urlPatterns = "/my")
 
+// HttpServlet 클래스를 상속한다 = 서블릿클래스를 생성한다
 public class MyServlet extends HttpServlet {
 
         @Override
+        // doGet() : 브라우저의 주소를 직접 변경해서 접근하는 경우에 호출되는 메소드
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
             resp.setContentType("text/html");
 
+            // 일반적인 자바의 sout = 서블릿의 PrintWriter 객체
             PrintWriter out = resp.getWriter();
             out.println("<html><body>");
             out.println("<h1>My Servlet</h1>");
---
+++ w1/src/main/webapp/test.jsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: HYOEUN
+  Date: 24. 4. 9.
+  Time: 오후 8:19
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<%-- jsp 파일은 화면을 구성하는데 사용한다--%>
+<%-- 파일이름 그대로 호출하기 --%>
+<body>
+<h1> Test JSP PAGE </h1>
+</body>
+</html>
