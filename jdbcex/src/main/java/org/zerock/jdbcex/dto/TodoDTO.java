98000738fd24b40c7f3c64efcad34c5083a0172c
Hyoeun01 <shyoeun09@gmail.com>
2024-04-10 20:28:41 +0900

    TodoDTO.java - Lombok를 이용해서 구성

---
+++ jdbcex/src/main/java/org/zerock/jdbcex/dto/TodoDTO.java
@@ -0,0 +1,13 @@
+package org.zerock.jdbcex.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Data // getter,setter,toString,equals,hashCode등을 모두 컴파일할 때 생성함
+@NoArgsConstructor
+@AllArgsConstructor
+public class TodoDTO {
+}
