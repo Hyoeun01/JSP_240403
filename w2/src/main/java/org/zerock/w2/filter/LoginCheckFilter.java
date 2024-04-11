7eb163fa8f8a3499f6c461301d4f230822be8863
Hyoeun01 <shyoeun09@gmail.com>
2024-04-11 17:24:57 +0900

    UTF8필터(적용안되는 문제있음) / 로그아웃컨트롤러 만들고 버튼눌러서 로그아웃> 다시 /todo/list 호출시 /login 으로 이동확인

--- w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
+++ w2/src/main/java/org/zerock/w2/filter/LoginCheckFilter.java
@@ -4,6 +4,9 @@ import lombok.extern.log4j.Log4j2;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
 @WebFilter(urlPatterns = {"/todo/*"})
@@ -12,6 +15,16 @@ public class LoginCheckFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         log.info("login check filter.................");
+        // /todo로 시작하는 모든 자원에 접근할때 체크필터가 동작하므로
+        // 로그인 여부를 체크하도록 한다
+        HttpServletRequest req = (HttpServletRequest) servletRequest;
+        HttpServletResponse resp = (HttpServletResponse) servletResponse;
+
+        HttpSession session = req.getSession();
+        if (session.getAttribute("loginInfo") == null) {
+            resp.sendRedirect( "/login");
+            return;
+        }
 
         filterChain.doFilter(servletRequest, servletResponse);
     }
--- w2/src/main/webapp/WEB-INF/todo/list.jsp
+++ w2/src/main/webapp/WEB-INF/todo/list.jsp
@@ -26,6 +26,9 @@
         </li>
     </c:forEach>
 </ul>
+<form action="/logout" method="post">
+    <button>LOGOUT</button>
+</form>
 
 </body>
 </html>
