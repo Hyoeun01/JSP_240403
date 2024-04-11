7110fa4e539283c83d230388b2ea095670c4c364
Hyoeun01 <shyoeun09@gmail.com>
2024-04-11 10:41:33 +0900

    log4j2.xml 파일 제작, 리스트컨트롤러 만들고 listAll() 기능 추가하기

--- jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
+++ jdbcex/src/main/java/org/zerock/jdbcex/service/TodoService.java
@@ -1,11 +1,16 @@
 package org.zerock.jdbcex.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.modelmapper.ModelMapper;
 import org.zerock.jdbcex.dao.TodoDAO;
 import org.zerock.jdbcex.domain.TodoVO;
 import org.zerock.jdbcex.dto.TodoDTO;
 import org.zerock.jdbcex.util.MapperUtil;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Log4j2
 public enum TodoService {
     INSTANCE;
 
@@ -23,10 +28,29 @@ public enum TodoService {
 
         // TodoDTO를 파라미터로 받아서 TodoVO로 변환하는 과정 필요
         TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
-        // ModelMapper로 처리된 TodoVO를 println으로 확인
-        System.out.println("todoVO: "+todoVO);
 
+        /*
+        ModelMapper로 처리된 TodoVO를 println으로 확인
+        System.out.println("todoVO: "+todoVO);
+        */
+
+        log.info(todoVO);
         // TodoDAO를 이용해서 insert()실행해서 TodoVO 등록
         dao.insert(todoVO); // int를 반환하므로 이를 이용해서 예외 처리도 가능
+    }
+
+    // TodoDAO에서 가져온 TodoVO의 목록을 모두 TodoDTO로 변환해서 반환
+    // ModelMapper와 자바스트림의 map()을 이용
+    public List<TodoDTO> listAll() throws Exception {
+        List<TodoVO> voList = dao.selectAll();
+        log.info("volist......................");
+
+        log.info(voList);
+
+        List<TodoDTO> dtoList = voList.stream()
+                .map(vo -> modelMapper.map(vo, TodoDTO.class))
+                .collect(Collectors.toList());
+
+        return dtoList;
     }
 }
--- jdbcex/src/test/java/org/zerock/service/TodoServiceTests.java
+++ jdbcex/src/test/java/org/zerock/service/TodoServiceTests.java
@@ -1,5 +1,6 @@
 package org.zerock.service;
 
+import lombok.extern.log4j.Log4j2;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.zerock.jdbcex.dto.TodoDTO;
@@ -7,6 +8,7 @@ import org.zerock.jdbcex.service.TodoService;
 
 import java.time.LocalDate;
 
+@Log4j2
 public class TodoServiceTests {
     private TodoService todoService;
 
@@ -22,7 +24,12 @@ public class TodoServiceTests {
                     .dueDate(LocalDate.now())
                     .build();
 
+            log.info("-----------------"); //테스트코드의 Log4j2설정확인
+            log.info(todoDTO);
+
             todoService.register(todoDTO);
+
+            //10:12:44.390 [Test worker] INFO  org.zerock.jdbcex.service.TodoService - TodoVO(tno=null, title=JDBC test title, dueDate=2024-04-11, finished=false) >>  log4j2.xml 에 로그 루트레벨을 info로 설정했기때문
         }
     }
 
