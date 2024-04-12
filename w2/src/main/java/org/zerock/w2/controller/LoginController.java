cd0d3fd16de562dcab16de6fd8ac03b7ab96b36c
Hyoeun01 <shyoeun09@gmail.com>
2024-04-12 11:32:23 +0900

    자동로그인 설정하기

--- w2/src/main/java/org/zerock/w2/controller/LoginController.java
+++ w2/src/main/java/org/zerock/w2/controller/LoginController.java
@@ -11,6 +11,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
+import java.util.UUID;
 
 @WebServlet("/login")
 @Log
@@ -32,6 +33,13 @@ public class LoginController extends HttpServlet {
         String mid = req.getParameter("mid");
         String mpw = req.getParameter("mpw");
 
+        String auto = req.getParameter("auto");
+
+        boolean rememberMe = auto != null && auto.equals("on");
+
+        if(rememberMe){
+            String uuid = UUID.randomUUID().toString();
+        }
         try {
             MemberDTO memberDTO = MemberService.INSTANCE.login(mid,mpw);
 
