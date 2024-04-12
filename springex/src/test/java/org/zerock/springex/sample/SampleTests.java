f3c6529130d513c76bccc94f6b2083660fb1e3d3
Hyoeun01 <shyoeun09@gmail.com>
2024-04-12 17:04:20 +0900

    HikariDataSource 만들고 설정하기

--- springex/src/test/java/org/zerock/springex/sample/SampleTests.java
+++ springex/src/test/java/org/zerock/springex/sample/SampleTests.java
@@ -8,6 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
+
 @Log4j2
 @ExtendWith(SpringExtension.class) // JUnit5버전에서 spring-test 이용하기위함. (JUnit4버전은 @Runwith)
 @ContextConfiguration(locations ="file:src/main/webapp/WEB-INF/root-context.xml") // 스프링의 설정 정보 로딩. 지금은 XML 설정이므로 locations를 이용하고 자바 설정의 경우 classes 속성 이용
@@ -17,10 +21,22 @@ public class SampleTests {
     @Autowired
     private SampleService sampleService;
 
+    @Autowired
+    private DataSource dataSource;
+
     @Test
     public void testService1() {
         // 실행시 스프링에서 생성하고 관리하는 객체 확인가능
         log.info(sampleService);
         Assertions.assertNotNull(sampleService);
+    }
+
+    @Test
+    public void testConnection() throws Exception {
+        Connection conn = dataSource.getConnection();
+        log.info(conn);
+        Assertions.assertNotNull(conn);
+
+        conn.close();
     }
 }
