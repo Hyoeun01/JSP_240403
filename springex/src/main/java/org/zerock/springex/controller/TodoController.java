ca1df7f7a82047944f58808ac244d4b191af2c77
Hyoeun01 <shyoeun09@gmail.com>
2024-04-16 11:38:26 +0900

    get/post방식을 구분해서 처리하도록 @GetMapping,@PostMapping 이용하기

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,6 +2,8 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -15,9 +17,19 @@ public class TodoController {
         log.info("todo list............");
     }
 
-    @RequestMapping(value = "/register", method = RequestMethod.GET) // /todo/register
+    /* @RequestMapping(value = "/register", method = RequestMethod.GET) // /todo/register
     public void register(){
         log.info("todo register...........");
     }
+    */
 
+    @GetMapping("/register")
+    public void registerGET(){
+        log.info("GET todo register...........");
+    }
+
+    @PostMapping("/register")
+    public void registerPOST(){
+        log.info("POST todo register...........");
+    }
 }
