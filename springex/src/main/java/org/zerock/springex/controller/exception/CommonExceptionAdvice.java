ad93350ac7c4e65381e041da548d172130682de6
Hyoeun01 <shyoeun09@gmail.com>
2024-04-16 14:57:54 +0900

    ResponseBody, ExceptionHandler 설명 추가

--- springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
+++ springex/src/main/java/org/zerock/springex/controller/exception/CommonExceptionAdvice.java
@@ -9,7 +9,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Log4j2
 public class CommonExceptionAdvice {
 
+    // 만들어진 문자열을 그대로 브라우저에 전송 - JSON 데이터도 전송가능
     @ResponseBody
+    //해당타입의 예외를 파라미터로 전달받을 수 있음
     @ExceptionHandler(NumberFormatException.class)
     public String exceptNumber(NumberFormatException numberFormatException) {
 
