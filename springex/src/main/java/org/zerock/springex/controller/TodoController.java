51c6a8474ca845d4dfbdf6fdd70b5b881bfeda61
Hyoeun01 <shyoeun09@gmail.com>
2024-04-16 11:36:30 +0900

    TodoController 에 RequestMapping 이용해서 컨트롤러 줄여보기

---
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -0,0 +1,23 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/todo")
+@Log4j2
+public class TodoController {
+
+    @RequestMapping("/list")
+    public void list() { // 최종경로 /todo/list
+        log.info("todo list............");
+    }
+
+    @RequestMapping(value = "/register", method = RequestMethod.GET) // /todo/register
+    public void register(){
+        log.info("todo register...........");
+    }
+
+}
