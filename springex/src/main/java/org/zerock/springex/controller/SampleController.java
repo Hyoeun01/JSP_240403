d88c3aa98a234c890bedc0f7022b96efefd0bba6
Hyoeun01 <shyoeun09@gmail.com>
2024-04-16 11:08:58 +0900

    servlet-context.xml 에 component-scan 추가하고 hello.jsp 만들기 >> 컨트롤러 클래스들을 스프링으로 인식하게 하기위함

---
+++ springex/src/main/java/org/zerock/springex/controller/SampleController.java
@@ -0,0 +1,16 @@
+package org.zerock.springex.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller // 해당 클래스가 스프링MVC에서 컨트롤러 역할을 하고, 스프링의 빈(bean)으로 처리되기 위함
+@Log4j2
+public class SampleController {
+
+    // GET방식으로 들어오는 요청을 처리하기 위해서 사용. /hello 라는 경로를 호출할 때 동작
+    @GetMapping("/hello")
+    public void hello() {
+        log.info("hello................................");
+    }
+}
