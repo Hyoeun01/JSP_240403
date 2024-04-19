6554a2e93f848435895180f4b0b6e996bb2bc842
Hyoeun01 <shyoeun09@gmail.com>
2024-04-19 09:46:49 +0900

    register.jsp 수정하기

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -2,6 +2,7 @@ package org.zerock.springex.controller;
 
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,7 +15,7 @@ import org.zerock.springex.dto.TodoDTO;
 public class TodoController {
 
     @RequestMapping("/list")
-    public void list() { // 최종경로 /todo/list
+    public void list(Model model) { // 최종경로 /todo/list
         log.info("todo list............");
     }
 
