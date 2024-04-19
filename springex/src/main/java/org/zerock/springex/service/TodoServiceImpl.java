00c6aaaa4a36b5115ecfffa277e034f2976611f6
Hyoeun01 <shyoeun09@gmail.com>
2024-04-19 09:23:39 +0900

    TodoService 인터페이스 작성하고, 서비스를 구현할 Impl 작성(스프링 빈으로 처리)하기

---
+++ springex/src/main/java/org/zerock/springex/service/TodoServiceImpl.java
@@ -0,0 +1,35 @@
+package org.zerock.springex.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.modelmapper.ModelMapper;
+import org.springframework.stereotype.Service;
+import org.zerock.springex.domain.TodoVO;
+import org.zerock.springex.dto.TodoDTO;
+import org.zerock.springex.mapper.TodoMapper;
+
+@Service
+@Log4j2
+
+// 클래스의 모든 필수(non-null) 속성을 초기화하는 생성자를 생성할 때 사용한다.
+// 이 생성자는 클래스의 모든 final 필드 또는 @NonNull 로 표시된 필드를 인자로 받아 초기화
+@RequiredArgsConstructor
+
+public class TodoServiceImpl implements TodoService {
+
+    // 의존성 주입이 필요한 객체의 타입을 final로 고정
+    private final TodoMapper todoMapper;
+    private final ModelMapper modelMapper;
+
+    @Override
+    public void register(TodoDTO todoDTO) {
+        log.info(modelMapper);
+
+        // modelMapper를 이용해서 TodoDTO를 TodoVO로 변환하고 todoMapper 를 통해서 insert 처리
+        TodoVO todoVO = modelMapper.map(todoDTO, TodoVO.class);
+
+        log.info(todoVO);
+
+        todoMapper.insert(todoVO);
+    }
+}
