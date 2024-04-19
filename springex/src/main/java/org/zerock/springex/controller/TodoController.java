7b4787eb796e5d061ed6443e65a851fd9a7b8d95
Hyoeun01 <shyoeun09@gmail.com>
2024-04-19 12:48:16 +0900

    @Valid를 이용한 TodoDTO 검증하기

--- springex/src/main/java/org/zerock/springex/controller/TodoController.java
+++ springex/src/main/java/org/zerock/springex/controller/TodoController.java
@@ -1,8 +1,10 @@
 package org.zerock.springex.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +15,8 @@ import org.zerock.springex.dto.TodoDTO;
 @Controller
 @RequestMapping("/todo")
 @Log4j2
+@RequiredArgsConstructor
+
 public class TodoController {
 
     @RequestMapping("/list")
@@ -32,8 +36,17 @@ public class TodoController {
     }
 
     @PostMapping("/register")
-    public String registerPOST(TodoDTO todoDTO, RedirectAttributes redirectAttributes){
+    public String registerPOST(TodoDTO todoDTO, BindingResult bindingResult, RedirectAttributes redirectAttributes){
         log.info("POST todo register...........");
+        // 검증에 문제가 있다면 다시 입력화면으로 리다이렉트 되게 함
+        if(bindingResult.hasErrors()){
+            log.info("has errors,,,,,,,,,,,,,");
+
+            // 처리과정에서 잘못된 결과는 RedirectAttributes의 addFlashAttribute()를 이용해서 전달
+            redirectAttributes.addFlashAttribute("errors", bindingResult.getAllErrors());
+
+            return "redirect:/todo/register";
+        }
         log.info(todoDTO);
 
         return "redirect:/todo/list";
