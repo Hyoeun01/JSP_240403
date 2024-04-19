36f98bf23c553aa8ce2c5c5de27b73e462b6da8a
Hyoeun01 <shyoeun09@gmail.com>
2024-04-19 09:28:18 +0900

    TodoService 테스트해보기

---
+++ springex/src/test/java/org/zerock/springex/service/TodoServiceTests.java
@@ -0,0 +1,33 @@
+package org.zerock.springex.service;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.zerock.springex.dto.TodoDTO;
+
+import java.time.LocalDate;
+
+@Log4j2
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = "file:src/main/webapp/WEB-INF/root-context.xml")
+public class TodoServiceTests {
+
+    @Autowired
+    private TodoService todoService;
+
+    @Test
+    public void testRegister(){
+
+        // 새로운 Todo를 등록할 때는 제목, 만료일, 작성자가 필요하므로 TodoDTO로 구성해서 TodoServiceImpl을 테스트
+        TodoDTO todoDTO = TodoDTO.builder()
+                .title("Test......")
+                .dueDate(LocalDate.now())
+                .writer("user1")
+                .build();
+
+        todoService.register(todoDTO);
+    }
+}
