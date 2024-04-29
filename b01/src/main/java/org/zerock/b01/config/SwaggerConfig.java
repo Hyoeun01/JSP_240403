cfa55da556eb627c4480f47d9e382cae18302ddb
Hyoeun01 <shyoeun09@gmail.com>
2024-04-29 10:47:55 +0900

    책에있는대로 안돼서 수정2차

--- b01/src/main/java/org/zerock/b01/config/SwaggerConfig.java
+++ b01/src/main/java/org/zerock/b01/config/SwaggerConfig.java
@@ -1,6 +1,7 @@
 package org.zerock.b01.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -9,12 +10,12 @@ import org.springframework.context.annotation.Configuration;
 
 public class SwaggerConfig {
 
-//  @Bean
-//  public OpenAPI openAPI() {
-//    return new OpenAPI()
-//        .info(new Info()
-//            .title("커머스 프로젝트 API")
-//            .description("상품을 등록하고, 상품을 장바구니에 담는 기능을 제공합니다.")
-//            .version("1.0.0"));
-//  }
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("레스트 API 테스트 ")
+                        .description("댓글을 이용해서  REST 방식 테스트 하겠다.")
+                        .version("1.0.0"));
+    }
 }
