bbd56df70ecbfd262803bf0c7d7bee31617b6a92
Hyoeun01 <shyoeun09@gmail.com>
2024-04-29 12:29:17 +0900

    댓글 등록 유효성 체크 ( 내용 비워서 send한뒤 에러 발생 확인해보기 )

--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -1,9 +1,12 @@
 package org.zerock.b01.controller;
 
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.zerock.b01.dto.ReplyDTO;
 
 
+import java.util.HashMap;
 import java.util.Map;
 
 @RestController
@@ -23,9 +27,16 @@ public class ReplyController {
     // ResponseEntity : 데이터를 HTTP 상태코드와 같이 앞단에 전달하는역할
     // JSON 타입의 문자열로 전달시 사용
     // 서버에 도착하면 JSON > ReplyDTO 타입으로 자동변환( jackson 라이브러리가 컨버터역할을 해서 - 스프링부트 자동내장)
-    public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO) {
+    public ResponseEntity<Map<String, Long>> register(@Valid @RequestBody ReplyDTO replyDTO, BindingResult bindingResult) throws BindException { // 오류 및 유효성 체크 등 오류 발생하면 오류 표시하게끔
         log.info(replyDTO);
-        Map<String, Long> resultMap = Map.of("rno",123L);
+
+        if(bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
+        Map<String, Long> resultMap = new HashMap<>();
+        resultMap.put("rno",1234L);
+
         return ResponseEntity.ok(resultMap);
     }
 }
