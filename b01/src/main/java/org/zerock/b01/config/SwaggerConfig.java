5794465e4ea50fb146d9c12a5838a3f6f1e39589
Hyoeun01 <shyoeun09@gmail.com>
2024-04-29 12:21:38 +0900

    rest방식의 컨트롤러가 에러가 났을 때 어디서 어떤 에러가 발생했는지 알아보게 하기위한 CustomRestAdvice 클래스 작성

--- b01/src/main/java/org/zerock/b01/config/SwaggerConfig.java
+++ b01/src/main/java/org/zerock/b01/config/SwaggerConfig.java
@@ -1,4 +1,5 @@
-package org.zerock.b01.config;import io.swagger.v3.oas.models.OpenAPI;
+package org.zerock.b01.config;
+import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
--- b01/src/main/java/org/zerock/b01/controller/ReplyController.java
+++ b01/src/main/java/org/zerock/b01/controller/ReplyController.java
@@ -21,6 +21,8 @@ public class ReplyController {
     @Tag(name="Replies POST", description = "POST 방식으로 댓글 등록")
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     // ResponseEntity : 데이터를 HTTP 상태코드와 같이 앞단에 전달하는역할
+    // JSON 타입의 문자열로 전달시 사용
+    // 서버에 도착하면 JSON > ReplyDTO 타입으로 자동변환( jackson 라이브러리가 컨버터역할을 해서 - 스프링부트 자동내장)
     public ResponseEntity<Map<String, Long>> register(@RequestBody ReplyDTO replyDTO) {
         log.info(replyDTO);
         Map<String, Long> resultMap = Map.of("rno",123L);
