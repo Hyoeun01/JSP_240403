7cdb4d22e032ef22a2a093dd144b3b29c01cbd8b
Hyoeun01 <shyoeun09@gmail.com>
2024-04-29 10:37:41 +0900

    책에있는대로 안돼서 수정

--- b01/src/main/java/org/zerock/b01/config/CustomServletConfig.java
+++ b01/src/main/java/org/zerock/b01/config/CustomServletConfig.java
@@ -2,8 +2,19 @@ package org.zerock.b01.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
-public class CustomServletConfig {
+public class CustomServletConfig implements WebMvcConfigurer {
+    // 정적파일 및 폴더 추가
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/js/**").addResourceLocations("classpath:/static/js/");
+        registry.addResourceHandler("/fonts/**").addResourceLocations("classpath:/static/fonts/");
+        registry.addResourceHandler("/css/**").addResourceLocations("classpath:/static/css/");
+        registry.addResourceHandler("/assets/**").addResourceLocations("classpath:/static/assets");
+    }
 }
