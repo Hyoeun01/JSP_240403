0167fb130b9db853db0de21b82849c40e26d809c
Hyoeun01 <shyoeun09@gmail.com>
2024-04-30 16:30:08 +0900

    Member 페이지 구성중...

---
+++ b01/src/test/java/org/zerock/b01/repsitory/MemberRepositoryTests.java
@@ -0,0 +1,32 @@
+package org.zerock.b01.repsitory;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.zerock.b01.domain.Member;
+import org.zerock.b01.repository.MemberRepository;
+
+import java.time.LocalDateTime;
+
+@SpringBootTest
+@Log4j2
+public class MemberRepositoryTests {
+    @Autowired
+    private MemberRepository memberRepository;
+
+    @Test
+    public void testInsert(){
+        Member member = Member.builder()
+                .member_id("test1")
+                .member_pw("1234")
+                .name("testuser1")
+                .phone("010-1234-5678")
+                .email1("test")
+                .email2("naver.com")
+                .agree(true)
+                .build();
+
+        Member result = memberRepository.save(member);
+    }
+}
