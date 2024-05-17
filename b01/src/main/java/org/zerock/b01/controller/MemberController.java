c17c26a83531d5f5a37fd50274904759179e1776
Hyoeun01 <shyoeun09@gmail.com>
2024-05-17 11:18:55 +0900

    회원가입하면 데이터베이스에 저장되는지 확인하기

--- b01/src/main/java/org/zerock/b01/controller/MemberController.java
+++ b01/src/main/java/org/zerock/b01/controller/MemberController.java
@@ -6,13 +6,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 import org.zerock.b01.dto.MemberJoinDTO;
+import org.zerock.b01.service.MemberService;
 
 @Controller
 @Log4j2
 @RequestMapping("/member")
 @RequiredArgsConstructor
 public class MemberController {
+    private final MemberService memberService;
 
     @GetMapping("/login")
     public void loginGET(String errorCode, String logout) {
@@ -30,10 +33,16 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinPOST(MemberJoinDTO memberJoinDTO) {
+    public String joinPOST(MemberJoinDTO memberJoinDTO, RedirectAttributes redirectAttributes) {
         log.info("join post..................");
         log.info(memberJoinDTO);
-
-        return "redirect:/board/list";
+        try{
+            memberService.join(memberJoinDTO);
+        }catch(Exception e) {
+            redirectAttributes.addFlashAttribute("error", "mid");
+            return "redirect:/member/join";
+        }
+        redirectAttributes.addFlashAttribute("result", "success");
+        return "redirect:/member/login";
     }
 }
