166e4d8defc01e4d255cc4c5076f4cb848d77d1f
Hyoeun01 <shyoeun09@gmail.com>
2024-05-17 15:37:31 +0900

    로그인 후처리하기

--- b01/src/main/java/org/zerock/b01/config/CustomSecurityConfig.java
+++ b01/src/main/java/org/zerock/b01/config/CustomSecurityConfig.java
@@ -12,10 +12,12 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.access.AccessDeniedHandler;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
 import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 import org.zerock.b01.security.CustomUserDetailsService;
 import org.zerock.b01.security.handler.Custom403Handler;
+import org.zerock.b01.security.handler.CustomSocialLoginSuccessHandler;
 
 import javax.sql.DataSource;
 
@@ -28,6 +30,11 @@ public class CustomSecurityConfig {
 
     private final DataSource dataSource;
     private final CustomUserDetailsService userDetailsService;
+
+    @Bean
+    public AuthenticationSuccessHandler authenticationSuccessHandler(){
+        return new CustomSocialLoginSuccessHandler(passwordEncoder());
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -47,7 +54,7 @@ public class CustomSecurityConfig {
                 .tokenValiditySeconds(60*60*24*30);
 
         http.exceptionHandling().accessDeniedHandler(accessDeniedHandler()); // 403
-        http.oauth2Login().loginPage("/member/login");
+        http.oauth2Login().loginPage("/member/login").successHandler(authenticationSuccessHandler());
         return http.build();
     }
 
@@ -69,5 +76,7 @@ public class CustomSecurityConfig {
     public AccessDeniedHandler accessDeniedHandler (){
         return new Custom403Handler();
     }
+
+
 
 }
