f693c0ba4cd5a09c671b9fbef0ba1fbbf3e1327f
Hyoeun01 <shyoeun09@gmail.com>
2024-05-17 09:26:19 +0900

    MemberRepository 만들고 테스트하기

--- b01/src/main/java/org/zerock/b01/repository/MemberRepository.java
+++ b01/src/main/java/org/zerock/b01/repository/MemberRepository.java
@@ -1,11 +1,18 @@
 package org.zerock.b01.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.zerock.b01.domain.Member2;
+import org.zerock.b01.domain.Member;
 
-public interface MemberRepository extends JpaRepository<Member2, String> {
-    @Query(value = "SELECT member_id,member_pw, name, phone, email1, email2, gender, agree, create_date from member where member_id = ?1 AND member_pw = ?2" , nativeQuery = true)
-    Member2 findByIdAndPassword(String member_id, String member_pw);
 
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, String> {
+//    @Query(value = "SELECT member_id,member_pw, name, phone, email1, email2, gender, agree, create_date from member2 where member_id = ?1 AND member_pw = ?2" , nativeQuery = true)
+//    Member2 findByIdAndPassword(String member_id, String member_pw);
+
+    @EntityGraph(attributePaths = "roleSet")
+    @Query("select m from Member m where m.mid = :mid and m.social = false ")
+    Optional<Member> getWithRoles(String mid);
 }
