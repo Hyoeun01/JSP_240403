fd80024fa7502bd6c22fbc95b5f79a19280017b0
Hyoeun01 <shyoeun09@gmail.com>
2024-05-24 10:32:55 +0900

    ajax호출 확인해보기 준비

--- api01/src/main/java/org/zerock/api01/config/CustomServletConfig.java
+++ api01/src/main/java/org/zerock/api01/config/CustomServletConfig.java
@@ -2,10 +2,18 @@ package org.zerock.api01.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableWebMvc
 public class CustomServletConfig implements WebMvcConfigurer {
     // 나중에 파라미터 타입의 변환을 추가할 수 있도록 생성해 둔다
+
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/files/**")
+                .addResourceLocations("classpath:/static/");
+    }
 }
