6c128ec294707600bec6f29aa3eefd376337eac2
Hyoeun01 <shyoeun09@gmail.com>
2024-05-29 12:22:36 +0900

    서비스계층과 컨트롤러 구현하고 확인해보기

--- api01/src/main/java/org/zerock/api01/controller/TodoController.java
+++ api01/src/main/java/org/zerock/api01/controller/TodoController.java
@@ -5,6 +5,8 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import org.zerock.api01.domain.Todo;
+import org.zerock.api01.dto.PageRequestDTO;
+import org.zerock.api01.dto.PageResponseDTO;
 import org.zerock.api01.dto.TodoDTO;
 import org.zerock.api01.service.TodoService;
 
@@ -28,5 +30,10 @@ public class TodoController {
     public TodoDTO read(@PathVariable("tno") Long tno) {
         log.info("read tno : " + tno);
         return todoService.read(tno);
+    }
+
+    @GetMapping(value = "/list", produces = MediaType.APPLICATION_JSON_VALUE)
+    public PageResponseDTO<TodoDTO> list(PageRequestDTO pageRequestDTO) {
+        return todoService.list(pageRequestDTO);
     }
 }
