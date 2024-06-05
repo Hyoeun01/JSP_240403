425b201a2c14496d28aeef6997ffd1e5a0410e68
Hyoeun01 <shyoeun09@gmail.com>
2024-06-05 13:36:10 +0900

    AWS S3서비스 알아보기

---
+++ appS3/src/test/java/org/zerock/apps3/util/S3UploaderTest.java
@@ -0,0 +1,24 @@
+package org.zerock.apps3.util;
+
+import lombok.extern.log4j.Log4j2;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+@Log4j2
+public class S3UploaderTest {
+    @Autowired
+    private S3Uploader s3uploader;
+
+    @Test
+    public void testUpload() {
+        try {
+            String filePath = "C:\\files\\100.jpg";
+            String uploadName = s3uploader.upload(filePath);
+            log.info(uploadName);
+        } catch (Exception e) {
+            log.error(e.getMessage());
+        }
+    }
+}
