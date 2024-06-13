6c9cc166e96f20c0593a6817b15eafe137010567
Hyoeun01 <shyoeun09@gmail.com>
2024-06-13 12:13:00 +0900

    CORS에러 해결을 위한 CrossOrigin 추가

--- SpringBackend/src/main/java/com/mysite/springbackend/controller/UserController.java
+++ SpringBackend/src/main/java/com/mysite/springbackend/controller/UserController.java
@@ -3,14 +3,12 @@ package com.mysite.springbackend.controller;
 import com.mysite.springbackend.entity.User;
 import com.mysite.springbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
+@CrossOrigin("http://localhost:3000")
 public class UserController {
     @Autowired
     private UserRepository userRepository;
@@ -23,7 +21,7 @@ public class UserController {
         return userRepository.save(user);
     }
 
-    @GetMapping("/users")
+    @GetMapping("/user")
     public List<User> getAllUsers() {
         return userRepository.findAll();
     }
