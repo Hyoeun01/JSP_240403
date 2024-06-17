646fada1cac57a529eec66c8e4372b3676276dfb
Hyoeun01 <shyoeun09@gmail.com>
2024-06-17 14:31:29 +0900

    시큐리티 설정중

---
+++ ShopBackend/src/main/java/com/example/shopbackend/util/SecurityUtils.java
@@ -0,0 +1,12 @@
+package com.example.shopbackend.util;
+
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+public class SecurityUtils {
+    public static final String ROLE_PREFIX = "ROLE_";
+
+    public static SimpleGrantedAuthority convertToAuthority(String role) {
+        String formatRole = role.startsWith(ROLE_PREFIX)? role: ROLE_PREFIX + role;
+        return new SimpleGrantedAuthority(formatRole);
+    }
+}
