329668d1b79946904608ea6a8f2567d9baa04ba2
Hyoeun01 <shyoeun09@gmail.com>
2024-06-17 16:40:41 +0900

    예외, 컨트롤러 수정

--- SpringBackend/src/main/java/com/mysite/springbackend/controller/UserController.java
+++ SpringBackend/src/main/java/com/mysite/springbackend/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.mysite.springbackend.controller;
 
 import com.mysite.springbackend.entity.User;
+import com.mysite.springbackend.exception.UserNotFoundException;
 import com.mysite.springbackend.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -24,5 +25,30 @@ public class UserController {
     @GetMapping("/users")
     public List<User> getAllUsers() {
         return userRepository.findAll();
+    }
+
+    @GetMapping("/user/{id}")
+    public User getUserById(@PathVariable Long id){
+        return userRepository.findById(id).orElseThrow(()->new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable Long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(()-> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUser(@PathVariable Long id){
+        if(!userRepository.existsById(id)){
+            throw new UserNotFoundException(id);
+        }
+        userRepository.deleteById(id);
+        return "유저아이디: "+id+" 삭제";
     }
 }
