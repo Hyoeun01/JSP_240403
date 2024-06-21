78e046054ea3dabbdb9a529f468dc693eed33e74
Hyoeun01 <shyoeun09@gmail.com>
2024-06-21 12:23:05 +0900

    필터에 @component 없어도 @autowired 되는것 확인, 컨트롤러&인증서비스 작성

--- ShopBackend/src/main/java/com/example/shopbackend/security/jwt/JwtAuthorizationFilter.java
+++ ShopBackend/src/main/java/com/example/shopbackend/security/jwt/JwtAuthorizationFilter.java
@@ -7,12 +7,11 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import java.io.IOException;
 
-@Component
+
 public class JwtAuthorizationFilter extends OncePerRequestFilter {
     @Autowired
     private JwtProvider jwtProvider;
